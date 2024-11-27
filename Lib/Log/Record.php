bc9d1dfb0cbd2d77bf9eaa3075ce4d4aa8c46ff2
ADAM <adamrpdiass@hotmail.com>
2024-11-27 10:02:17 -0300

    feat:criteria

--- Lib/Log/Record.php
+++ Lib/Log/Record.php
@@ -69,19 +69,7 @@ abstract class Record
         {
             Transaction::log($sql);
             $result = $conn->query($sql);
-        }
-        else
-        {
-            throw new Exception('Não há transação ativa');
-        }
-    }
-    public function store()
-    {
-        if ($conn = Transaction::get())
-        {
-            Transaction::log($sql);
-            $result = $conn->query($sql);
-            if($result)
+            if ($result)
             {
                 return $result->fetchObject(get_class($this));
             }
@@ -91,17 +79,102 @@ abstract class Record
             throw new Exception('Não há transação ativa');
         }
     }
-
-    public function delete()
+    public function store()
     {
+        if (empty($this->data['id']) OR (!$this->load($this->data['id'])))
+        {
+            $prepared = $this->prepare($this->data);
+            if(empty($this->data['id']))
+            {
+                $this->data['id'] = $this->getLast() + 1;
+                $prepared['id'] = $this->data['id'];
+            }
+            $sql = "INSERT INTO {$this->getEntity()}" .
+                    '(' . implode(', ', array_keys($this->data)).')' .
+                    'values ' .
+                    '('. implode(', ', array_values($this->data)) .')';
+        }
+        else
+        {
+            $prepared = $this->prepare($this->data);
+            
+            $set = [];
+            foreach ($prepared as $column => $value) {
+                $set[] = "$column = $value";
+            }
+            $sql = "UPDATE {$this->getEntity()}";
+            $sql .= "SET " . implode(' , ', $set);
+            $sql .= "WHERE id= " . (int)$this->data['id'];
+        }
         if ($conn = Transaction::get())
         {
             Transaction::log($sql);
-            $result = $conn->query($sql);
+            return $conn->exec($sql);
+            /*if($result)
+            {
+                return $result->fetchObject(get_class($this));
+            }*/
         }
         else
         {
             throw new Exception('Não há transação ativa');
+        }
+
+    }
+
+    public function delete($id = null)
+    {
+        $id = $id ? $id : $this->data['id'];
+
+        $sql = "DELETE FROM {$this->getEntity()} WHERE id=" . (int) $id;
+        if ($conn = Transaction::get())
+        {
+            Transaction::log($sql);
+            return $conn->exec($sql);
+        }
+        else
+        {
+            throw new Exception('Não há transação ativa');
+        }
+    }
+    public function getlast()
+    {
+        if ($conn = Transaction::get())
+        {
+            $sql = "SELECT max(id) FROM {$this->getEntity()}";
+            Transaction::log($sql);
+            $result = $conn->query($sql);
+            $row = $result->fetch();
+            return $row[0];
+        }
+        else
+        {
+            throw new Exception('Não há transação ativa');
+        }
+    }
+
+    public function prepare($data)
+    {
+        $prepared = array();
+        foreach($data as $key => $value){
+            if(is_scalar($value)){
+                $prepared[$key] = $this->escape($value);
+            }
+        }
+        return $prepared;
+    }
+
+    public function escape($value)
+    {
+        if(is_string($value) and (!empty($value)))
+        {
+            // adiciona \ em aspas
+            $value = addslashes($value);
+            return "'$value'";
+        }
+        else if (is_bool($value))
+        {
+            return $value ? 'TRUE' : 'FALSE';
         }
     }
 }
