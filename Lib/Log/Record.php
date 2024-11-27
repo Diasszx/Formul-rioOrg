424e22772fd2101f1aba762a9a7e38d0c334aaad
ADAM <adamrpdiass@hotmail.com>
2024-11-27 08:33:07 -0300

    feat: record

--- Lib/Log/Record.php
+++ Lib/Log/Record.php
@@ -3,6 +3,18 @@
 abstract class Record
 {
     protected $data;
+
+    public function __construct($id = null)
+    {
+        if($id)
+        {
+            $object = $this->load($id);
+            if($object)
+            {
+                $this->fromArray($object->toArray());
+            }
+        }
+    }
     public function __set($prop, $value)
     {
          if ($value === NULL)
@@ -26,6 +38,71 @@ abstract class Record
     public function __isset($prop)
     {
         return isset($this->data[$prop]);
+    }
+
+    public function __clone()
+    {
+        unset($this->data['id']);
+    }
+
+    public function fromArray($data)
+    {
+        $this->data = $data;
+    }
+
+    public function toArray()
+    { 
+        return $this->data;
+    }
+
+    public function getEntity()
+    {
+        $class = get_class($this);
+        constant("{$class}::TABLENAME");
+    }
+
+    public function load($id)
+    {
+        $sql = "SELECT * FROM {$this->getEntity()} WHERE id=" . (int) $id;
+        
+        if ($conn = Transaction::get())
+        {
+            Transaction::log($sql);
+            $result = $conn->query($sql);
+        }
+        else
+        {
+            throw new Exception('Não há transação ativa');
+        }
+    }
+    public function store()
+    {
+        if ($conn = Transaction::get())
+        {
+            Transaction::log($sql);
+            $result = $conn->query($sql);
+            if($result)
+            {
+                return $result->fetchObject(get_class($this));
+            }
+        }
+        else
+        {
+            throw new Exception('Não há transação ativa');
+        }
+    }
+
+    public function delete()
+    {
+        if ($conn = Transaction::get())
+        {
+            Transaction::log($sql);
+            $result = $conn->query($sql);
+        }
+        else
+        {
+            throw new Exception('Não há transação ativa');
+        }
     }
 }
 
